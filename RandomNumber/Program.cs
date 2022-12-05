fcf8d3b24324898a90b6e29c99c7459e4d9ba97c
Akash Khan <akash4gb@gmail.com>
2022-12-05 23:03:17 +0600

    Add project files.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿using Random = RandomNumber.Random;
+
+Console.WriteLine("Random number generator:");
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine($"{i}. {Convert.ToBase64String(Random.GenerateRandomNumbers(32))}");
+}
---
+++ RandomNumber/Random.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography;
+
+namespace RandomNumber;
+public class Random
+{
+    public static byte[] GenerateRandomNumbers(int length)
+    {
+        using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+        byte[] randomNumber = new byte[length];
+        randomNumberGenerator.GetBytes(randomNumber);
+        return randomNumber;
+
+    }
+}
